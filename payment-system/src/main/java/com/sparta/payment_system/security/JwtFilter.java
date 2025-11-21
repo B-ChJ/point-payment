124ea49f64998b65270a24459670cbd1feefe9c8
B-ChJ <bchaejoo@gmail.com>
2025-11-21 12:28:29 +0900

    fix: 필터에서 refresh 토큰 검증 책임 제거(분리) - 필터 통과(하드코딩) 제거 - 회원가입 후 authToken 값이 없는 상태에서 login.html → point-payment.html로 자동 이동되는 로직에 조건문 추가(token 유무 검증)

--- payment-system/src/main/java/com/sparta/payment_system/security/JwtFilter.java
+++ payment-system/src/main/java/com/sparta/payment_system/security/JwtFilter.java
@@ -2,10 +2,8 @@ package com.sparta.payment_system.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -25,53 +23,16 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String path = request.getServletPath();
 
-        if (path.equals("/login.html") || path.equals("/register.html") || path.equals("/point-payment.html")
-                || path.startsWith("/api/auth/login") || path.startsWith("/api/auth/register")
-                || path.startsWith("/api/product")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        String accessToken = resolveAccessToken(request);
+        String bearerToken = request.getHeader("Authorization");
+        String accessToken = jwtUtil.resolveAccessToken(bearerToken);
 
         //AccessToken Filter
         if (StringUtils.hasText(accessToken) && jwtUtil.validateAccessToken(accessToken)) {
             Authentication authentication = jwtUtil.getAuthentication(accessToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        else {
-            String refreshToken = resolveRefreshToken(request);
-            if(refreshToken != null && jwtUtil.validateRefreshToken(refreshToken)) {
-                String newAccessToken = jwtUtil.createAccessToken(jwtUtil.getAuthentication(refreshToken));
-                response.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + newAccessToken);
-                }
-            else {
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-        }
 
         filterChain.doFilter(request, response);
-    }
-
-    private String resolveAccessToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
-    }
-
-    private String resolveRefreshToken(HttpServletRequest request) {
-        if(request.getCookies() != null) {
-            for(Cookie cookie : request.getCookies()) {
-                if(StringUtils.hasText(cookie.getName()) && cookie.getName().equals("refreshToken")) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null;
     }
 }
